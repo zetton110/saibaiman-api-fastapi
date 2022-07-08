9348f58ff942575ebf6e124a60c7ab39c89de536
itoh <kohei.ito@nulab.com>
2022-07-08 10:31:47 +0900

    :+1: Improve to delete upload file with record

--- backend/app/api/routes/snapshots.py
+++ backend/app/api/routes/snapshots.py
@@ -6,27 +6,10 @@ from app.models.snapshot import SnapshotCreate, SnapshotPublic
 from fastapi import APIRouter, Body, Depends, HTTPException, Path, UploadFile, File
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 import shutil
+import os
 
 router = APIRouter()
 
-@router.get('/',
-            response_model=List[SnapshotPublic],
-            name='snapshots:get-all-snapshots')
-async def get_all_snapshots(
-    snapshots_repo: SnapshotsRepository = Depends(get_repository(SnapshotsRepository))
-) -> List[SnapshotPublic]:
-    return await snapshots_repo.get_all_snapshots()
-
-@router.post('/',
-             response_model=SnapshotPublic,
-             name='snapshots:create-snapshot',
-             status_code=HTTP_201_CREATED)
-async def create_new_snapshot(
-    new_snapshot: SnapshotCreate = Body(..., embed=True),
-    snapshots_repo: SnapshotsRepository = Depends(get_repository(SnapshotsRepository)),
-) -> SnapshotPublic:
-    created_snapshot = await snapshots_repo.create_snapshot(new_snapshot=new_snapshot)
-    return created_snapshot
 
 @router.get('/{id}/', response_model=SnapshotPublic,
             name='snapshots:get-snapshot-by-id')
@@ -42,24 +25,45 @@ async def get_snapshot_by_id(
             detail='Not found')
     return snapshot
 
+@router.get('/',
+            response_model=List[SnapshotPublic],
+            name='snapshots:get-all-snapshots')
+async def get_all_snapshots(
+    snapshots_repo: SnapshotsRepository = Depends(get_repository(SnapshotsRepository))
+) -> List[SnapshotPublic]:
+    return await snapshots_repo.get_all_snapshots()
+
 @router.delete('/{id}/', response_model=int, name='snapshots:delete-snapshot-by-id')
 async def delete_snapshot_by_id(
     id: int = Path(..., ge=1, title='The ID of the snapshot to delete.'),
     snapshots_repo: SnapshotsRepository = Depends(get_repository(SnapshotsRepository))
 ) -> int:
-    deleted_id = await snapshots_repo.delete_snapshot_by_id(id=id)
+    snaphot = await snapshots_repo.get_snapshot_by_id(id=id)
+    os.remove(f'uploads/{snaphot.image_file}')
+    deleted_id = await snapshots_repo.delete_snapshot_by_id(id=snaphot.id)
     if not deleted_id:
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND,
             detail='No snapshot found with that id.')
     return deleted_id
 
-@router.post('/upload')
+@router.post('/upload/image')
 def uploadfile(upload_file: UploadFile = File(...)):
     path = f'uploads/{upload_file.filename}'
     with open(path, 'w+b') as buffer:
         shutil.copyfileobj(upload_file.file, buffer)
     return {
-        'filename': path,
+        'filename': upload_file.filename,
         'type': upload_file.content_type
     }
+
+@router.post('/upload/meta',
+             response_model=SnapshotPublic,
+             name='snapshots:create-snapshot',
+             status_code=HTTP_201_CREATED)
+async def create_new_snapshot(
+    new_snapshot: SnapshotCreate = Body(..., embed=True),
+    snapshots_repo: SnapshotsRepository = Depends(get_repository(SnapshotsRepository)),
+) -> SnapshotPublic:
+    created_snapshot = await snapshots_repo.create_snapshot(new_snapshot=new_snapshot)
+    return created_snapshot
